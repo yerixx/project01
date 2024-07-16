431729114e894ae7ded360ca40425dee8ee44581
yerixxz <yerixxz@gmail.com>
2024-07-16 21:13:31 +0900

    240716

--- subproject/slide page/js/script.js
+++ subproject/slide page/js/script.js
@@ -1,23 +1,13 @@
-const tabs = document.querySelectorAll(".tab");
-const pics = document.querySelectorAll(".pics");
-// console.log(tabs, pics);
+const tabs = document.querySelectorAll(".content");
 
-pics.forEach((img) => {
-  img.addEventListener("click", function () {
-    //console.log("click");
-    pics.forEach((sibling) => {
-      if (sibling !== img) {
-        sibling.classList.remove("active");
-      }
+tabs.forEach((item, index) => {
+  item.addEventListener("click", function () {
+    const pics = document.querySelectorAll(".pic");
+
+    pics.forEach((img) => {
+      img.classList.remove("active");
     });
-    this.classList.add("active");
 
-    //연결고리 만들기
-    const targetId = img.getAttribute("data-alt");
-    console.log(targetId);
-    const targetContent = document.getElementById(targetId);
-    if (targetContent) {
-      targetContent.classList.add("active");
-    }
+    pics[index].classList.add("active");
   });
 });
